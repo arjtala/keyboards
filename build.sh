82486fb1be7e6b351a14f21accaa5e237c4f6b07
Arjang Talattof <talattof@gmail.com>
2022-09-19 20:21:02 -0700

    updated image size

--- build.sh
+++ build.sh
@@ -6,8 +6,8 @@ LAYOUT_FILE=./ferris_compact_v01.json
 CURRENTDIR=`pwd`
 
 mkdir -p $QMK_FIRMWARE_DIR/keyboards/ferris/keymaps/$LAYOUT/
-cp keymap.c $QMK_FIRMWARE_DIR/keyboards/ferris/keymaps/$LAYOUT/keymap.c
-cp $LAYOUT_FILE $QMK_FIRMWARE_DIR/keyboards/ferris/keymaps/$LAYOUT/$LAYOUT_FILE
+cp layouts/$LAYOUT/keymap.c $QMK_FIRMWARE_DIR/keyboards/ferris/keymaps/$LAYOUT/keymap.c
+cp layouts/$LAYOUT/$LAYOUT_FILE $QMK_FIRMWARE_DIR/keyboards/ferris/keymaps/$LAYOUT/$LAYOUT_FILE
 
 cd $QMK_FIRMWARE_DIR
 qmk compile -kb ferris/0_2/compact -km $LAYOUT
